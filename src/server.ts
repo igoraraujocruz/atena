a263ca74133d57f2406458385adaa73a043045f8
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-06-21 12:44:08 -0300

    feat: project base

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import '../src/database';
+import express from 'express';
+import morgan from 'morgan';
+
+require('dotenv/config');
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/', (request, response) =>
+  response.json({
+    message: process.env.APP_NAME,
+  }),
+);
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server started on port ${process.env.APP_PORT}`);
+});
